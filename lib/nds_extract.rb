ba703c599246d50e210784bdce8e813d1540e0ab
Scott Matsuda <smatsuda2014@gmail.com>
2019-11-02 20:45:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -38,7 +38,7 @@ def total_gross(source)
   total = 0
   index = 0
   while index < source.length do
-    total += directors_totals(source)
+    total += directors_totals(source)[list_of_directors(source)[index]]
     index += 1
   end
   total
