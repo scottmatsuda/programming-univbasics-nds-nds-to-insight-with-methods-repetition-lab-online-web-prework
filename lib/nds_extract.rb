d9eaf42592849a79e8fe6c0579e38277fd7e6cf4
Scott Matsuda <smatsuda2014@gmail.com>
2019-11-02 19:18:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,7 +25,12 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  name = []
+  index = 0
+  while index < source.length do
+    name << source[index][:name]
+  end
+  name
 end
 
 def total_gross(source)
